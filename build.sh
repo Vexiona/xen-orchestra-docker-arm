a2f027cfafcb577d856c5ec1e20598c4ecf9b131
Ian Nicolae <88843406+Vexiona@users.noreply.github.com>
2024-06-23 00:22:06 +0300

    update for compose build

--- build.sh
+++
@@ -1,8 +0,0 @@
-export BUILDKIT_STEP_LOG_MAX_SIZE=20485760;
-export DOCKER_BUILDKIT=1;
-docker buildx build \
---platform=linux/arm64 \
---progress=plain \
---build-arg BUILDKIT_STEP_LOG_MAX_SIZE=20485760 \
--t ghcr.io/andreitelteu/xen-orchestra-docker-arm \
-.
